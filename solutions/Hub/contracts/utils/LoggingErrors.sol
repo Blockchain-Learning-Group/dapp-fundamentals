ce674d649fa915a19c37ede90a2c684301db163d
Adam J Lemmon <adamjlemmon@gmail.com>
2017-10-04 07:52:18 -0400

    complete hub for eod2 added

---
+++ solutions/Hub/contracts/utils/LoggingErrors.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.11;
+
+/**
+ * @title Log Various Error Types
+ * @author Adam Lemmon <adam@blockchainlearninggroup.com>
+ * @dev Inherit this contract and your may now log errors easily
+ * To support various error types, params, etc.
+ */
+contract LoggingErrors {
+  /**
+  * Events
+  */
+  event LogErrorString(string errorString);
+
+  /**
+  * Error cases
+  */
+
+  /**
+   * @dev Default error to simply log the error message and return
+   * @param _errorMessage The error message to log
+   * @return ALWAYS false
+   */
+  function error(string _errorMessage) internal returns(bool) {
+    LogErrorString(_errorMessage);
+    return false;
+  }
+}
