2016e964b01b2b703f419c0f8217d27d154e64c7
Adam J Lemmon <adamjlemmon@gmail.com>
2017-12-21 11:38:49 -0500

    update conf.py

---
+++ conf.py
@@ -0,0 +1,8 @@
+import sys, os
+
+sys.path.append(os.path.abspath('course-content'))
+
+source_suffix = '.rst'
+master_doc = 'index.rst'
+project = 'Blockchain Learning Group DApp Fundamentals'
+copyright = '2017, Blockchain Learning Group Inc.'
