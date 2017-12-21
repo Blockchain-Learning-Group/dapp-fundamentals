5418f770cf83aad6b637b3f01d10584977e6b05b
Adam J Lemmon <adamjlemmon@gmail.com>
2017-12-21 12:03:19 -0500

    correct extensions

--- conf.py
+++ conf.py
@@ -1,8 +1,9 @@
 import sys, os
 
-sys.path.append(os.path.abspath('course-content'))
+# sys.path.append(os.path.abspath('course-content'))
 
+extensions = ['course-content']
 source_suffix = '.rst'
-master_doc = 'index'
+master_doc = 'course-content/index'
 project = 'Blockchain Learning Group DApp Fundamentals'
 copyright = '2017, Blockchain Learning Group Inc.'
