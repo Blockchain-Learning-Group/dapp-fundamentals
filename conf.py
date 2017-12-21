de8985f4d4b1125ac32cb5e01320f5cfed789d07
Adam J Lemmon <adamjlemmon@gmail.com>
2017-12-21 11:14:55 -0500

    remove latex files

--- conf.py
+++ conf.py
@@ -192,7 +192,6 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-        ('index',)
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
