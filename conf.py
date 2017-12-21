65381006fec7fde11b94f48ac30eb22fccc9939c
Adam J Lemmon <adamjlemmon@gmail.com>
2017-12-21 11:39:54 -0500

    fix index extension

--- conf.py
+++ conf.py
@@ -3,6 +3,6 @@ import sys, os
 sys.path.append(os.path.abspath('course-content'))
 
 source_suffix = '.rst'
-master_doc = 'index.rst'
+master_doc = 'index'
 project = 'Blockchain Learning Group DApp Fundamentals'
 copyright = '2017, Blockchain Learning Group Inc.'
