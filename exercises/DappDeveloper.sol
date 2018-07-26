ed6e900dc3ffd133f50b333e6fe0fd421f002db6
Adam J Lemmon <adamjlemmon@gmail.com>
2018-07-26 11:05:40 -0400

    update dapp dev template to remove modifiers

--- exercises/DappDeveloper.sol
+++ exercises/DappDeveloper.sol
@@ -2,16 +2,16 @@ pragma solidity ^0.4.24;
 
 contract DappDeveloper {
   // Storage variables
-  string public name_;
-  string public currentLocation_;
+  string name_;
+  string currentLocation_;
 
   // Set your name, @param _name Your name.
-  constructor(string _name) public {
+  constructor(string _name) {
     name_ = _name;
   }
 
   // Set your current location, @param _location Current location.
-  function setLocation(string _location) external {
+  function setLocation(string _location) {
     currentLocation_ = _location;
   }
 }
