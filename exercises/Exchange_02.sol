62d93aea83bafc89eb7766b6e923643551404db3
Adam J Lemmon <adamjlemmon@gmail.com>
2018-07-27 13:25:37 -0400

    strip down exchange template

--- exercises/Exchange_02.sol
+++ exercises/Exchange_02.sol
@@ -28,41 +28,33 @@ contract Exchange {
   ) external
   {
     // Sufficent token balance, allowance, given to the exchange
-    require(Token(_bidToken).allowance(msg.sender, this) >= _bidAmount, "Insufficient allowance given.");
+
 
     // Confirm order does not already exist
-    bytes32 orderId = keccak256(msg.sender, _bidToken, _bidAmount, _askToken, _askAmount);
-    require(orderBook_[orderId].askAmount == 0, "Order already exists."); // check for existence, default to 0, assume no one is giving tokens away for free
+
 
     // Add order to the order book
-      orderBook_[orderId] = Order({
-        maker: msg.sender,
-        bidToken: _bidToken,
-        bidAmount: _bidAmount,
-        askToken: _askToken,
-        askAmount: _askAmount
-      });
+
 
     // Emit Event
-    emit OrderSubmitted(orderId, msg.sender, _bidToken,_bidAmount, _askToken, _askAmount);
+
   }
 
   /// @dev Execute an order that has been matched. NOTE msg.sender is the taker. Only allows complete fills.
   function executeOrder (bytes32 _orderId) external payable {
     // Load the order into mem, save gas on read operations
-    Order memory order = orderBook_[_orderId];
+
 
     // Confirm the taker sent the correct balance
-    require(msg.value == order.askAmount);
+
 
     // Execute the trade
-    order.maker.transfer(order.askAmount);
-    require(Token(order.bidToken).transferFrom(order.maker, msg.sender, order.bidAmount), "transferFrom failed.");
+
 
     // Remove the order
-    delete orderBook_[_orderId];
+
 
     // Emit Event
-    emit OrderExecuted(_orderId, order.maker, msg.sender, order.bidToken, order.bidAmount, order.askToken, order.askAmount);
+
   }
 }
