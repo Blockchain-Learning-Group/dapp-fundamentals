f4c058342b33f19d80ebf0daa4f0524b4f96c1d8
Abhinav Boyed <abhinav.b5415@gmail.com>
2018-08-06 21:53:28 -0400

    added commented exercises and solutions

--- solutions/BatchSend.sol
+++ solutions/BatchSend.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.4.24;
 contract BatchSend {
     event BatchSent(address[] addresses);
 
+//create a constructor function here with following
+//properties: external, payable and which returns a boolean
+//params array of addresses called _addresses and array of uint256 called _values
+
     function batchSend(
         address[] _addresses, 
         uint256[] _values
@@ -11,12 +15,16 @@ contract BatchSend {
         payable
         returns(bool) 
     {
-
+        // create a requirement that each address is associated to an value
+        // HINT: think about the respective lengths
         require(_addresses.length == _values.length, "Sanity check, array lengths do not match");
 
-        // Counter to ensure no value from contract being used
+        // assign a variable remaining value to the value being sent by the msg
         uint256 remainingValue = msg.value;
 
+        // create a loop that will use the .transfer method to send the value
+            // require that the remainingValue in contract is greated that value remaining
+            //decrement the value in the in the remaining value variable
         for (uint8 i = 0; i < _addresses.length; i++) {
             require(remainingValue >= _values[i], "Insufficient ether sent to fill the batch");
             _addresses[i].transfer(_values[i]);
@@ -28,6 +36,7 @@ contract BatchSend {
             msg.sender.transfer(remainingValue);
         }   
 
+        //emit an event which sends the addresses that have been paid out
         emit BatchSent(_addresses);
         return true;
     }
--- solutions/HodlCapsule.sol
+++ solutions/HodlCapsule.sol
@@ -2,16 +2,22 @@ pragma solidity 0.4.24;
 
 
 contract Capsule {
+
+    //declare the following variables: owner_, amount_ and unlocktime_
     address public owner_;
     uint256 public amount_;
     uint public unlockTime_;
 
+    // create a constructor function that makes this contract payable
+        // it should assign the variables you just declared
     constructor() public payable {
         owner_ = msg.sender;
         amount_ = msg.value;
         unlockTime_ = now + 3 minutes;
     }
 
+    // create a send function that will send the ether back to the owner once the unlocktime has been reached
+        // use the selfdestruct function to revert the contract
     function send() external {
         require(msg.sender == owner_, "msg.sender != owner");
         require(now >= unlockTime_, "Capsule not unlocked yet.");
