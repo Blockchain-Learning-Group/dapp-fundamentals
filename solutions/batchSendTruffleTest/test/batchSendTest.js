f5577aa02c1ae95513ba91fde0ae6404afc661c2
Abhinav Boyed <aboyed@Abhinavs-MacBook-Pro.local>
2018-08-17 16:10:50 -0400

    tried completing truffle test for batchSend

--- solutions/batchSendTruffleTest/test/batchSendTest.js
+++ solutions/batchSendTruffleTest/test/batchSendTest.js
@@ -2,23 +2,32 @@ var batchSend = artifacts.require('./BatchSend.sol');
 expect = require('chai').expect;
 console.log(expect);
 contract('batchSend.GetMessage', function(accounts) {
-  
-    describe("Contract Should Deploy", function() {
-        it("Catch an instance of the batchSend Contract", async function(done) {
-            // testing creating the contact
-            var batch_send = await batchSend.new({ from: accounts[0]});
-            let owner = await batch_send.owner()
+    var owner = accounts[0];
+    var reciever = accounts[1];
 
-            expect(owner).to.be.equal(accounts[1]);
-            console.log(owner);
+    describe("Contract Should Deploy", function() {
+        it("catch an instance of the contract", function(){
+            // call the .new() function on the Demo artifact and use the .then function to assign the callback variable instance to demoContract
+            return batchSend.new().then(function(instance){
+                batch_send = instance;
+            });
         });
     });
 
     describe("batchSend function should work", function(){
         it("Call the batchSend function", async function(){
-            await batch_send.batchSend([accounts[1], accounts[2]], [10000, 20000], ({ from: accounts[0], value: 50000}))
+            var amountToBeSent = 100000;
+            var amountSent = 200000;
+            var ownerBalanceBefore = web3.getBalance(owner).toNumber();
+            var recieverBalanceBefore = web3.getBalance(reciever).toNumber();
+            await batch_send.batchSend(reciever, amountToBeSent, ({ from: owner, value: amountToBeSent}))
+            var recieverBalanceAfter = web3.getBalance(reciever).toNumber();
+            var ownerBalanceAfter = web3.getBalance(owner).toNumber();
+            
+            assert.equal(ownerBalanceBefore-ownerBalanceAfter, amountSent-amountToBeSent, "amount was not correctly taken from sender");
+            assert.equal(recieverBalanceAfter - recieverBalanceBefore, amountToBeSent);
         });
-        // to be added
+
 
     });
 
