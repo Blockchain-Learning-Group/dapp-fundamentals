3e82d76eccf63a359f52617824e8b22cebf7f86f
Adam J Lemmon <adamjlemmon@gmail.com>
2018-08-18 11:01:47 -0400

    soln name access

--- solutions/truffleExercise2/test/nameTest.js
+++ solutions/truffleExercise2/test/nameTest.js
@@ -1,33 +1,33 @@
-
 // include the Demo.sol contract and pass it as an artifcat to the Demo variable
-var Demo = artifacts.require("./Demo.sol");
-// initialized the chai assertion library
-assert = require("chai").assert;
+const Demo = artifacts.require("./Demo.sol");
 
 // use the contract object with to "Test the Demo contract" with a callback function with no params
-contract("Test the Demo contract", function(){
+contract("Test the Demo contract", function() {
     // use the describe function to "deploy the Demo smart contract" with a callback function with no params
-    describe("deploy the Demo smart contract", function(){
+    describe("deploy the Demo smart contract", () => {
         // use the it function to "catch an instance of the contract" with a callback function with no params
-        it("catch an instance of the contract", function(){
-            // call the .new() function on the Demo artifact and use the .then function to assign the callback variable instance to demoContract
-            return Demo.new().then(function(instance){
-                demoContract = instance;
-            });
+        it.only("catch an instance of the contract", async () => {
+            const demo = await Demo.new();
+            assert.isNotNull(demo, "contract not deloyed");
         });
     });
    
     // use the describe function to "Check the contract variable" with a callback function with no params
-    describe("Check the contract variable", function(){
+    describe("Check the contract variable", function() {
         // use the it function to ensure "The name variable is correct" with a callback function with no params
-        it("The name variable is correct", function(){
-            // return the .name() function of the demoContract object - call the .then() function with the result
-            return demoContract.name().then(function(res){
-                // console log the result
-                console.log(res.toString());
-                // expect the result to.be.equal to "shahrukh khan"
-                assert.equal(res, "shahrukh khan", "The name variable is not correctly set")
-            });
+        it("The name variable is correct", async () => {
+            const demo = await Demo.new();
+            console.log(await demo.name());
+            console.log(await demo.name());
+            console.log(await demo.name());
+
+            const name = await demo.name();
+
+            console.log(name);
+            console.log(name);
+            console.log(name);
+            // expect the result to.be.equal to "shahrukh khan"
+            assert.equal(name, "shahrukh khan", "The name variable is not correctly set");
         });
 
         it("Should change the name as expected", async () => {
@@ -42,5 +42,4 @@ contract("Test the Demo contract", function(){
             });
         })
     });
-
 })
